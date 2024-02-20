9d138fee2686840499ae6105d74e943818aa448f
SavioReis <72362096+SavioReis@users.noreply.github.com>
2024-02-20 18:20:10 -0300

    Add files via upload

---
+++ Ensures feitos em java/arma/client.js
@@ -0,0 +1,8 @@
+RemoveAllPedWeapons(PlayerPedId())
+
+RegisterCommand('kit',()=>{
+    GiveWeaponToPed(PlayerPedId(), -1355376991, 100, false, false)
+    GiveWeaponToPed(PlayerPedId(), 2138347493, 100, false, false)
+    GiveWeaponToPed(PlayerPedId(), 1119849093, 100, false, false)
+    GiveWeaponToPed(PlayerPedId(), -1813897027, 100, false, false)
+})
---
+++ Ensures feitos em java/forca/client.js
@@ -0,0 +1,7 @@
+RegisterCommand('superforca', ()=>{
+    SetWeaponDamageModifier('WEAPON_UNARMED',1000)
+})
+
+RegisterCommand('fraco', ()=>{
+    SetWeaponDamageModifier('WEAPON_UNARMED',1)
+})
